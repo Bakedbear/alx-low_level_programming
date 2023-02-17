896ead2ae0f13ea364f48898c6802af80eb32155
Bakedbear <Bakedbear78194@gmail.com>
2023-02-17 09:44:16 -0800

    this quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * Main - prints a string to standard error
+ * Return: returns 1 to main
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
