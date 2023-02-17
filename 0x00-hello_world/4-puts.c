975ac42d4d65e2e69ee077247cf3c720a4fb84cb
Bakedbear <Bakedbear78194@gmail.com>
2023-02-17 03:17:42 -0800

    added hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - outputs a string
+ * return - used to return 0 to main to exit program
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
