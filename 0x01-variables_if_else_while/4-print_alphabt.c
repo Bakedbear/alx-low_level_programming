6bbc78129a9708e4b6b4bab886b51bfe626dedec
Bakedbear <Bakedbear78194@gmail.com>
2023-02-20 02:39:31 -0800

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - program that prints the alphabet in lowercase and skips values q and e
+  * Return: 0 exit code of the program if successful
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x == 'q' || x == 'e')
+		{
+		    ;
+		}
+		else
+		{
+		    putchar(x);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
