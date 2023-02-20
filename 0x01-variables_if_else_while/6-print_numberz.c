8ebf889f4b54cb97bdc3a0f93f201b89e16871c6
Bakedbear <Bakedbear78194@gmail.com>
2023-02-20 02:58:07 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - Prints all single digit numbers of base 10 starting from 0
+  * Return: 0 Exit code when success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar((x % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
