e2ad8a0fd61d17d858f60eb9c3621390ec6f87db
Bakedbear <Bakedbear78194@gmail.com>
2023-02-20 02:33:21 -0800

    uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - program that prints the alphabet in lowercase then in uppercase
+  * Return: 0 exit code of the program if successful
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
