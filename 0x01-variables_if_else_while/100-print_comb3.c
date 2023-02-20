56acae6d03ab748bc4dccd65ab50ef07681715d8
Bakedbear <Bakedbear78194@gmail.com>
2023-02-20 09:29:41 -0800

    comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,25 +6,25 @@
  */
 int main(void)
 {
-	int n, m;
+        int n, m;
 
-	for (n = 48; n <= 57; n++)
-	{
-		for (m = 48; m <= 57; m++)
-		{
-		    	if (m > n && n != m)
-		    	{
-		    		putchar(n);
-		    		putchar(m);
-		    		if (n != 56 || m != 57)
-		    		{
-		    			putchar(',');
-		    			putchar(' ');
-		    		}
-		    	}
-			
-		}
-	}
-	putchar('\n');
-	return (0);
+        for (n = 48; n <= 57; n++)
+        {
+                for (m = 48; m <= 57; m++)
+                {
+                    if (m > n && n != m)
+                    {
+                        putchar(n);
+                        putchar(m);
+                        if (n != 56 || m != 57)
+                            {
+                                putchar(',');
+                                putchar('   ');
+                            }
+                    }
+                }
+        }
+
+        putchar('\n');
+        return (0);
 }
