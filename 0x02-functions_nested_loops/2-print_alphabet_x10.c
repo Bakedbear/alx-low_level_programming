a097808a93d5b8236608816c98c027f0729950c0
Bakedbear <Bakedbear78194@gmail.com>
2023-02-21 14:08:17 -0800

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints 10 times the alphabet in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int x, y;
+
+	for (y = 0; y <= 10; y++)
+	{
+		for(x = 'a'; x <= 'z'; x++)
+			{
+				_putchar(x);
+			}
+		_putchar('\n');
+	}
+	
+}
