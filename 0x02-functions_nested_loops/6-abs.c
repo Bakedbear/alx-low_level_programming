c5ec2e01956b230f141783985a1e0682774bbf17
Bakedbear <Bakedbear78194@gmail.com>
2023-02-22 03:48:01 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int _abs - computes absolute value of an integer
+ * @a: integer to be checked
+ * Return: absolute value
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return(a);
+	}
+	return (-a);
+}
