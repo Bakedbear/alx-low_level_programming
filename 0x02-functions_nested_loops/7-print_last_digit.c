47b1a2cf783c4955c04bdd395bddff9374fdbfa9
Bakedbear <Bakedbear78194@gmail.com>
2023-02-22 05:39:14 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,15 @@
 /**
  * print_last_digit - prints last digit of a number
  * @a: first integer value of the two-digit number 
- * @b: last digit value of the two-digit number
  * Return: last number
  */
 int print_last_digit(int a)
 {
 	int b;
-
 	if (a < 0)
 		a = -a;
 
-	b = a%10;
+	b = a % 10;
 	if (b < 0)
 		b = -b;
 	_putchar(b + '0');
