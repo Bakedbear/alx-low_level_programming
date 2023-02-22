54aafad5a34c7f5085c5b22740f5a99170c17c7f
Bakedbear <Bakedbear78194@gmail.com>
2023-02-22 07:26:48 -0800

    to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,15 +13,15 @@ void print_to_98(int n)
 	{
 		for (a = n; a <= 98; a++)
 			if (a != 98)
-				printf("%d ", a);
+				printf("%d ,", a);
 			else if (a == 98)
 				printf("%d", a);
 	}
 	else if (n >= 98)
 	{
-		for (b = n; b>= 98; b--)
+		for (b = n; b >= 98; b--)
 			if (b != 98)
-				printf("%d ", b);
+				printf("%d ,", b);
 			else if (b == 98)
 				printf("%d", b);
 	}
