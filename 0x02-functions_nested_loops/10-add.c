d8fe558626d3ec47237eef8b92422f9ec220fa16
Bakedbear <Bakedbear78194@gmail.com>
2023-02-22 06:17:01 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @x: first number
+ * @y: second number
+ * Return: added result
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
