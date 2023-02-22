4bce4216670bf867451ae17bc8ae3de296400a92
Bakedbear <Bakedbear78194@gmail.com>
2023-02-22 05:59:44 -0800

    24hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int x = 0, y;
+
+	while (x < 24)
+	{
+		y = 0;
+		while (y < 60)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
