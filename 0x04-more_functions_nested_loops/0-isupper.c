511bff3a097f992822c8c07a32ed339c237a37fb
Bakedbear <Bakedbear78194@gmail.com>
2023-02-27 03:28:35 -0800

    new commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
- * _isupper - function that checks for uppercase character
- * Return: 1 if c is uppercase or 0 if lowercase
- * @c: integer to be checked
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
