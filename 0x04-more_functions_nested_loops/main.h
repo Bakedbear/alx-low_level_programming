ed49b797627182f16bccf4f44c3e663251c41ac0
Bakedbear <Bakedbear78194@gmail.com>
2023-02-27 03:25:53 -0800

    updated main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,12 @@
-#define MAIN_H
-#ifndef MAIN_H
+#define _MAIN_H_
+#ifndef _MAIN_H_
 
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 int _putchar(char);
 void print_numbers(void);
+void print_number(int n);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
