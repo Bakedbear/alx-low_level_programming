a841b890034e31a4dd40fc0f42564ce3bc88f8c9
Bakedbear <Bakedbear78194@gmail.com>
2023-02-28 12:47:27 -0800

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
- * puts2 - prints out every other character in a string 
- * starting with the first character
- * @str: string
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
  */
 void puts2(char *str)
 {
