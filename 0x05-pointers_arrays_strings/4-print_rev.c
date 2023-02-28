ff39429499090fb7560ccdd4244b60583ad1c618
Bakedbear <Bakedbear78194@gmail.com>
2023-02-28 03:58:31 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse followed by new line
+ * @s: string to be reversed
+ */
+void print_rev(char *s)
+{
+	int i, len, temp;
+
+	len = strlen(s);
+
+	for (i = 0; i < len/2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
