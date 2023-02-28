fd40d00fa5944dec009ae715536182712e8a58b8
Bakedbear <Bakedbear78194@gmail.com>
2023-02-28 03:35:12 -0800

    length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
 /**
- * _strlen - returns length of string
+ * _strlen - returns the length of a string
  * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int i;
+	int a;
 
-	i = 0;
+	a = 0;
 
-	while (s[i] != '\0')
+	while (s[a] != '\0')
 	{
-		i++;
+		a++;
 	}
 
-	return (i)
+	return (a);
 }
