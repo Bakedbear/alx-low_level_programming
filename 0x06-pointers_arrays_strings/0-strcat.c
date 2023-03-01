5e58f9a99397d413c29f9cab1b8d1bc5864b580f
Bakedbear <Bakedbear78194@gmail.com>
2023-03-01 15:32:26 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *_strcat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * Return: pointer to resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	x = 0; 
+	y = 0;
+
+	while dest[x] != '\0'
+		x++;
+
+	while src[y] != '\0'
+	{
+		dest[x] = src[y];
+		y++;
+		x++;
+	}
+
+	dest[x] = '\0';
+
+	return (dest);
+}
