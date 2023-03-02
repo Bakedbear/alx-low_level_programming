a82702ce1af9c961798da90fa4188d935b586b15
Bakedbear <Bakedbear78194@gmail.com>
2023-03-02 12:59:15 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string using rot13
- * @s: string
- * Return: resulting string
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
  */
 char *rot13(char *s)
 {
 	int i, j;
 
-	char arr1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char arr2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (s[i] == arr1[j])
+			if (s[i] == a[j])
 			{
-				s[i] = arr2[j];
+				s[i] = b[j];
 				break;
 			}
 		}
