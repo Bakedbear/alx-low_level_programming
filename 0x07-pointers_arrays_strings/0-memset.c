076666d7c935e77526c5a6595e5a5e91e85c0d60
Bakedbear <Bakedbear78194@gmail.com>
2023-03-07 02:22:58 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,6 +6,7 @@
  * @n: number of times to copy
  * Return: pointer to memory area s 
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
@@ -14,6 +15,5 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[i] = b;
 	}
-
 	return (s);
 }
