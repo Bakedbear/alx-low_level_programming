cb6b544c4a315bc4e2afea0592409c2b8639d04e
Bakedbear <Bakedbear78194@gmail.com>
2023-03-07 03:13:49 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strspn - gets length of prefix substring
+ * @s: string to check initial segment
+ * @accept: string containing list of characters to match
+ *
+ * Return: number of bytes from segment s that matches  bytes in accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, flag, o;
+
+	o = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				o++;
+				flag = 1;
+			}
+		}
+		if (flag = 0)
+		{
+			return (o);
+		}
+	}
+
+	return (0);
+}
