ffebb3c48ebbb150e0a2383809ae36ffb02f03d6
Bakedbear <Bakedbear78194@gmail.com>
2023-03-09 07:25:16 -0800

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+int find_sqrt_recursion(n, i);
+/**
+ * _sqrt_recursion - returns natural square root of a number
+ * @n: number to get square root 
+ * Return: square root
+ * return -1 if number does not have natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (find_sqrt_recursion(n, 0));
+}
+
+/**
+ * find_sqrt_recursion - returns square root of a number
+ * @n: number provided
+ * @i: iterator
+ * Return: square root
+ */
+int find_sqrt_recursion(n, i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i = n)
+		return (i);
+	return find_sqrt_recursion(n, n + 1);
+}
