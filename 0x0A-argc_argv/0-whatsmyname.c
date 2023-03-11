2e6cf9929b9d69a57a611ac4739aef2b9754542c
Bakedbear <Bakedbear78194@gmail.com>
2023-03-11 11:02:00 -0800

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - prints name of program
+ * @argc: counts number of arguments in program
+ * @argv: collection of arrays containing the parameters
+ *
+ * Return: 0 successful
+ */
 int main(int argc, char* argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv[]);
+	return (0);
 }
