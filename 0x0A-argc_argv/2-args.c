c0468abd44869ec2c9e5f107980b2f3512ee4ac3
Bakedbear <Bakedbear78194@gmail.com>
2023-03-11 11:24:34 -0800

    arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments received
+ * @argc: number of arguments
+ * @argv: array of parameters
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
