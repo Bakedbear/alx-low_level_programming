709fd678a45185359eeba83eb49f4ad0ab24d863
Bakedbear <Bakedbear78194@gmail.com>
2023-03-11 11:11:25 -0800

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed to it
+ * @argc: number of arguments
+ * @argv: array containing actual parameters passed
+ * Return: 0 exit code
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
