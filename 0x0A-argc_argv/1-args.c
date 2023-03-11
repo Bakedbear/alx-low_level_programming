0e97891b7c3a869f80bf50c44e9ef0da64c45566
Bakedbear <Bakedbear78194@gmail.com>
2023-03-11 11:19:10 -0800

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: array containing actual parameters passed
  * Return: 0 exit code
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
