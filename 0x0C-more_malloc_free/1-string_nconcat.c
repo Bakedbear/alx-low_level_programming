78d7c32cef63641b1278ad721e66555b12f8083b
Bakedbear <Bakedbear78194@gmail.com>
2023-03-17 16:03:54 -0700

    added unsigned int

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
 		len1++;
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		ptr[i++] = s2[j++];
 	}
-	while (n >= len2 && i < (len1 + len2))
+	while (n >= len2 && i < (len1 + leni2))
 		ptr[i++] = s2[j++];
 
 	ptr[i] = '\0';
