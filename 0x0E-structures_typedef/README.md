1171a23bebb16c75dfd5211946524e9e8eec6fa8
Bakedbear <Bakedbear78194@gmail.com>
2023-03-21 15:31:33 -0700

    structures, typedef

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+## Structures, typedef
+0. define new type structure in C
+1. initialize variable 
+2. print struct
+3. give a type a new name
+4. create a new dog structure
+5. free dog structure from memory
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct dog - structure with three elements
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: structure named struct dog containing three elements
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
