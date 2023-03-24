226c0129896d06e515ee09bf2228862dec545851
Bakedbear <Bakedbear78194@gmail.com>
2023-03-23 19:32:17 -0700

    main.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: the array
+ * @size: size of the array
+ * @cmp: pointer to the function used to compare values
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "calc.h"
+
+/**
+ * op_add - returns sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of two integers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_add - subtracts two integers and returns the result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of subtraction of two integers
+ */
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * op_add - returns product of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of multiplication of two integers
+ */
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * op_add - returns result of division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of division of two integers
+ */
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_add - returns remainder of a divided by b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of division of a by b
+ */
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
+
+
+
+
+
