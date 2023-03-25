9080c6caf60921cc2924b796bceb34391f192895
Bakedbear <Bakedbear78194@gmail.com>
2023-03-25 07:10:25 -0700

    fixed trailing whitespace

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include "dog.h"
+
 /**
  * free_dog - frees memory allocated for struct dog
- * @d: struct to be freed 
+ * @d: struct to be freed
  */
 void free_dog(dog_t *d)
 {
