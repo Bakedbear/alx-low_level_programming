15f7b71d55f1b979ef42405e2518aff90933157c
Bakedbear <Bakedbear78194@gmail.com>
2023-04-04 18:13:50 -0700

    fixed initial error

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -6,10 +6,11 @@
  * @n: data to insert in the new element
  * Return: address of new element or NULL if it failed
  */
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *last;
-	listint_t *temp = *head
+	listint_t *temp = *head;
 
 	last = malloc(sizeof(listint_t));
 
